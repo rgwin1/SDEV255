96f826d60eac562ccac1a29a0ce04daafec89125
Ryan <rgwin1@ivytech.edu>
2025-02-03 00:51:08 -0500

    created code along files and directories: videos 1-9 complete

---
+++ Module3/CodeAlong/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; //thing that defines a structure of the documents that are going to be stored inside a model -- constructor function
+
+const blogSchema = new Schema({
+    //add different properties that blogs are going to have
+    title: {
+        type: String,
+        required: true
+    }, 
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Blog = mongoose.model('Blog', blogSchema); //first argument is name of the model, second argument is the schema
+
+
+module.exports = Blog;
--- Module3/CodeAlong/server.js
+++ Module3/CodeAlong/server.js
@@ -1,15 +1,73 @@
 const http = require('http');
-
+const fs = require('fs');
+const _ = require('lodash');
 
 
 const server = http.createServer((req, res) => { //req = request, and res = response
-    console.log('request made'); //not doing anything at this point, have to invoke listen method
+    //console.log('request made'); //not doing anything at this point, have to invoke listen method
+    
 
-})
+    //lodash get random number
+    const num = _.random(0, 20); //will give random number everytime server function runs
+    console.log(num)
+
+    const greet = _.once(() => {
+        console.log('Hello');
+
+    });
+
+    greet();
+    greet();
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html'); //sending back plain text to the browser
+
+    let path = './Views/';
+    //switch statement to cycle through the different cases
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //send an HTML file
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+            // res.end();
+        }
+    })
+    //send a file
 
 
+    // res.write('<head><link rel="stylesheet" href="#"></head>')
+    // res.write('<p>hello, ninjas</p>');
+    // res.write('<p>hello again, ninjas</p>');
+    // res.end() //have to end the response back to the browser
+    
+});
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000') //this is the port number, localhost = default value, and the function that runs when event is heard
+
+
 });
 
 
@@ -20,3 +78,5 @@ server.listen(3000, 'localhost', () => {
 //port number represents a specific channel, gateway, or door on our computer, that certain server software can send to?
 //server needs own port number (3000) is common for local web development, as long as nothing else is using it?
 //localhost:3000
+//ctrl+c cancels out of process in the terminal
+//this code wont run in the browser, it's running on the back end
